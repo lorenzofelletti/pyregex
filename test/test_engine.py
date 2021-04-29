5780c3fe4bc7f289457eb04ea5769e21c92847be
lorenzofelletti <lorenzo97.felletti@gmail.com>
2021-04-29 16:57:04 +0200

    added more tests

--- test/test_engine.py
+++ test/test_engine.py
@@ -17,3 +17,25 @@ def test_simplest_with_wildcard(reng):
 
 def test_simplest_but_longer(reng):
     assert (True, 3) == reng.match('a.c', 'abc')
+
+
+def test_wildcard(reng):
+    assert (True, 2) == reng.match('.*a', 'aa')
+
+
+def test_backtracking(reng):
+    assert (True, 4) == reng.match('a*a', 'aaaa')
+
+
+def test_or(reng):
+    assert (True, 1) == reng.match('a.*|b', 'b')
+
+
+def test_or_no_match(reng):
+    res, consumed = reng.match('a|b', 'c')
+    assert res == False
+
+
+def test_bt_no_match(reng):
+    res, consumed = reng.match('a{5}a', 'aaaaa')
+    assert res == False
