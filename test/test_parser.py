8fef9ac73e91b49b153360e85f9749e94aea897c
lorenzofelletti <lorenzo97.felletti@gmail.com>
2022-01-15 18:40:44 +0100

    added more tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,5 +1,5 @@
 import pytest
-from ..src.re_ast import RE, GroupNode, Element
+from ..src.re_ast import RE, EndElement, GroupNode, Element, RangeElement, SpaceElement, StartElement
 from ..src.pyrser import Pyrser
 
 
@@ -48,3 +48,32 @@ def test_fail_no_closing_par(parser):
 def test_parse_match_start_end(parser):
     ast = parser.parse('^aaaa.*a$')
     assert len(ast.child.children) == 8
+
+
+def test_complex_regex(parser):
+    ast = parser.parse('^[a-z|A-Z]{1,20}@[a-z|A-Z]\.[a-z]{1,3}$')
+    assert len(ast.child.children) == 7
+
+    assert type(ast.child.children[0]) is StartElement
+
+    assert type(ast.child.children[1]) is RangeElement
+    assert ast.child.children[1].min == 1
+    assert ast.child.children[1].max == 20
+
+    assert type(ast.child.children[2]) is Element
+
+    assert type(ast.child.children[3]) is RangeElement
+
+    assert type(ast.child.children[4]) is Element
+
+    assert type(ast.child.children[5]) is RangeElement
+    assert ast.child.children[5].min == 1
+    assert ast.child.children[5].max == 3
+
+    assert type(ast.child.children[6]) is EndElement
+
+
+def test_space_element(parser):
+    ast = parser.parse('\s')
+    assert len(ast.child.children) == 1
+    assert type(ast.child.children[0]) is SpaceElement
