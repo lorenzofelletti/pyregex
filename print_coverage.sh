4eae3817b58c22cab09f23ff963071eebe191f8f
lorenzofelletti <lorenzo97.felletti@gmail.com>
2022-01-25 16:47:09 +0100

    you can now pass coverage report options

--- print_coverage.sh
+++ print_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(source venv/bin/activate;coverage run --omit 'venv/*,test/*' -m pytest;coverage report;deactivate)
+(source venv/bin/activate;coverage run --omit 'venv/*,test/*' -m pytest;coverage report "$@";deactivate)
